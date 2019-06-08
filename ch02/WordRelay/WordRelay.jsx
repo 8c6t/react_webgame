f96e0896a69ceffaeb3069daedea18af6a5510ba
8c_6t <nkw8620@gmail.com>
2019-06-09 01:54:08 +0900

    2-8. 끝말잇기 Class 만들기

--- ch02/WordRelay/WordRelay.jsx
+++ ch02/WordRelay/WordRelay.jsx
@@ -3,11 +3,54 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: 'Hello, webpack',
+    word: '하루',
+    value: '',
+    result: '',
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    
+    const { word, value } = this.state;
+
+    if (word[word.length - 1] === value[0]) {
+      this.setState({
+        result: '정답',
+        word: value,
+        value: '',
+      });
+    } else {
+      this.setState({
+        result: '오답',
+        value: '',
+      });
+    }
+    this.input.focus();
+  } 
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  input;
+
+  onRefInput = (c) => {
+    this.input = c;
   }
 
   render() {
-    return <h1>{this.state.text}</h1>
+    const { word, value, result } = this.state;
+
+    return (
+      <>
+        <div>{word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input ref={this.onRefInput} value={value} onChange={this.onChangeInput} />
+          <button>입력</button>
+        </form>
+        <div>{result}</div>
+      </>
+    )
   }
 }
 
--- ch02/WordRelay/webpack.config.js
+++ ch02/WordRelay/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   name: 'word-relay-setting', // 웹팩 설정에 대한 이름
@@ -17,11 +18,24 @@ module.exports = {
       test: /\.jsx?/, // jsx 파일에
       loader: 'babel-loader', // babel-loader를 적용
       options: { // 바벨의 옵션
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+        presets: [
+          // @babel/preset-env에 대한 세부 옵션
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR' ], // browserlist
+            },
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
         plugins: ['@babel/plugin-proposal-class-properties'],
       },
     }],
   },
+  plugins: [
+    // 로더(module) 들에게 옵션을 추가해주는 플러그인
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+  ],
   // 출력
   output: {
     path: path.join(__dirname, 'dist'),
