4ef618e31a6a79cf5db2d8080483ddc97c736c4d
8c_6t <nkw8620@gmail.com>
2019-06-09 17:25:01 +0900

    3-3. 리액트 반복문(key)

--- ch03/NumberBaseball/NumberBaseball.jsx
+++ ch03/NumberBaseball/NumberBaseball.jsx
@@ -32,9 +32,18 @@ class NumberBaseball extends Component {
         </form>
         <div>시도: {tries.length}</div>
         <ul>
-          {['사과', '바나나', '포도', '귤'].map((v) => {
+          {[
+            { fruit: '사과', taste: '맛있다' },
+            { fruit: '감', taste: '시다' },
+            { fruit: '귤', taste: '달다' },
+            { fruit: '밤', taste: '떪다' },
+            { fruit: '배', taste: '맛있다' },
+            { fruit: '사과', taste: '맛없다' },
+          ].map((v) => {
+            /* index를 key로 쓰는 경우 성능 최적화에 문제가 있음 */
+            /* 요소가 추가만 되는 배열은 i를 사용해도 됨(삭제 X) */
             return (
-              <li>{v}</li>
+              <li key={v.fruit + v.taste}><b>{v.fruit}</b> - {v.taste}</li>
             )
           })}
         </ul>
