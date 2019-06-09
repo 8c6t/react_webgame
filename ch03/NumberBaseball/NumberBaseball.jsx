20905e34ebf5fb4411063a4e091f74554d4bc1b3
8c_6t <nkw8620@gmail.com>
2019-06-09 15:47:19 +0900

    3-1. import와 require 비교

---
+++ ch03/NumberBaseball/NumberBaseball.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NumberBaseball extends Component {
+
+}
+
+export const hello = 'hello';
+
+export default NumberBaseball;
+
+// const React = require('react');
+// exports.hello = 'hello';
+// module.exports = NumberBaseball;
---
+++ ch03/NumberBaseball/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  name: 'number-baseball',
+  mode: 'development',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: {
+    app: ['./client'],
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'],
+            },
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+        plugins: [
+          'react-hot-loader/babel'
+        ],
+      },
+    }],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist/',
+  },
+}
