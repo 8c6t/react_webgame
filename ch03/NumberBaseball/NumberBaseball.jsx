a1a399da1ec25f38e99757614c5090d672272191
8c_6t <nkw8620@gmail.com>
2019-06-09 17:10:40 +0900

    3-2. 리액트 반복분(map)

--- ch03/NumberBaseball/NumberBaseball.jsx
+++ ch03/NumberBaseball/NumberBaseball.jsx
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
 
+function getNumbers() {
+
+}
+
 class NumberBaseball extends Component {
 
+  state = {
+    result: '',
+    value: '',
+    answer: getNumbers(),
+    tries: [],
+  }
+
+  onSubmitForm = (e) => {
+
+  }
+
+  onChangeInput = (e) => {
+
+  }
+
+  render() {
+    const { result, value, answer, tries } = this.state;
+
+    return (
+      <>
+        <h1>{result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input maxLength={4} value={value} onChange={this.onChangeInput} />
+        </form>
+        <div>시도: {tries.length}</div>
+        <ul>
+          {['사과', '바나나', '포도', '귤'].map((v) => {
+            return (
+              <li>{v}</li>
+            )
+          })}
+        </ul>
+      </>
+    )
+  }
 }
 
 export const hello = 'hello';
