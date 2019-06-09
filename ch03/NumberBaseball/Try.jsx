a9cf4301a7d417270cbfbdebdb5ef194434c384b
8c_6t <nkw8620@gmail.com>
2019-06-10 02:16:03 +0900

    3-13. props와 state 연결하기

--- ch03/NumberBaseball/Try.jsx
+++ ch03/NumberBaseball/Try.jsx
@@ -1,6 +1,20 @@
-import React, { PureComponent, memo } from 'react';
+import React, { PureComponent, memo, useState } from 'react';
 
 /* class Try extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    // 다른 동작
+    const filtered = this.props.filter(() => {
+
+    });
+
+    this.state = {
+      result: filtered,
+      try: this.props.try,
+    };
+  }
+
   render() {
     const { tryInfo } = this.props;
     return (
@@ -12,11 +26,20 @@ import React, { PureComponent, memo } from 'react';
   }
 } */
 
-const Try = memo(({ tryInfo }) => (
-  <li>
-    <div>{tryInfo.try}</div>
-    <div>{tryInfo.result}</div>
-  </li>
-));
+const Try = memo(({ tryInfo }) => {
+  // 부모로부터 받은 props를 state로 만들어서 사용
+  const [result, setResult] = useState(tryInfo.result);
+
+  const onClick = () => {
+    setResult('1');
+  };
+
+  return (
+    <li>
+      <div>{tryInfo.try}</div>
+      <div onClick={onClick}>{result}</div>
+    </li>
+  )
+});
 
 export default Try;
