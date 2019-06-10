b7facdcf3aea576c6b097a721e567690ee11d3b7
8c_6t <nkw8620@gmail.com>
2019-06-10 13:02:18 +0900

    4-2. setTimeout 넣어 반응속도 체크

--- ch04/ResponseCheck/ResponseCheckClass.jsx
+++ ch04/ResponseCheck/ResponseCheckClass.jsx
@@ -7,8 +7,41 @@ class ResponseCheck extends Component {
     results: [],
   }
 
-  onClickScreen = () => {
+  timeout;
+  startTime;
+  endTime;
 
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요',
+      });
+
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭' 
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+    } else if (state === 'ready') {
+      this.setState({
+        state: 'waiting',
+        message: '초록색일 때 클릭하세요',
+      });
+      clearTimeout(this.timeout);
+    } else if (state === 'now') {
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작하세요',
+          results: [...prevState.results, this.endTime - this.startTime ],
+        }
+      });
+    }
   }
 
   renderAverage = () => {
