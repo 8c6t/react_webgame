5616fea3cc24299433f3330eb53adf41b05db340
8c_6t <nkw8620@gmail.com>
2019-06-11 14:41:24 +0900

    5-3. 가위바위보 게임 만들기

--- ch05/RockPaperScissors/RockPaperScissors.jsx
+++ ch05/RockPaperScissors/RockPaperScissors.jsx
@@ -12,6 +12,12 @@ const scores = {
   paper: -1,
 }
 
+const computerChoice = (imgCoord) => {
+  return Object.entries(rpsCoord).find(function(v) {
+    return v[1] === imgCoord;
+  })[0];
+};
+
 // constructor -> render -> ref -> componentDidMount
 // (setState/props 바뀔 시 -> shouldComponentUpdate(true) -> render -> componentDidUpdate)
 // 부모 컴포넌트에서 제거 시 -> componentWillUnmount -> 소멸
@@ -27,22 +33,7 @@ class RockPaperScissors extends Component {
   // 컴포넌트가 첫 렌더링된 뒤 -> 비동기 요청 처리
   componentDidMount() {
     // const { imgCoord } = this.state; // -142px. 클로저 -> 비동기 함수가 바깥의 변수를 참조하는 경우...
-    this.interval = setInterval(() => {
-      const { imgCoord } = this.state;
-      if (imgCoord === rpsCoord.rock) {
-        this.setState({
-          imgCoord: rpsCoord.scissor
-        });
-      } else if (imgCoord === rpsCoord.scissor) {
-        this.setState({
-          imgCoord: rpsCoord.paper
-        });
-      } else if (imgCoord === rpsCoord.paper) {
-        this.setState({
-          imgCoord: rpsCoord.rock
-        });
-      }
-    }, 1000);
+    this.interval = setInterval(this.changeHand, 100);
   }
 
   // 리렌더링 후
@@ -55,9 +46,54 @@ class RockPaperScissors extends Component {
     clearInterval(this.interval);
   }
 
-  onClickBtn = () => {
+  onClickBtn = (choice) => {
+    const { imgCoord } = this.state;
 
-  }
+    clearInterval(this.interval);
+    const myScore = scores[choice];
+    const cpuScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+
+    if (diff === 0) {
+      this.setState({
+        result: '비겼습니다'
+      });
+    } else if ([-1, 2].includes(diff)) {
+      this.setState((prevState) => {
+        return {
+          result: '이겼습니다',
+          score: prevState.score + 1
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          result: '졌습니다',
+          score: prevState.score - 1,
+        };
+      });
+    }
+    setTimeout(() => {
+      this.interval = setInterval(this.changeHand, 100);
+    }, 2000);
+  };
+
+  changeHand = () => {
+    const { imgCoord } = this.state;
+    if (imgCoord === rpsCoord.rock) {
+      this.setState({
+        imgCoord: rpsCoord.scissor
+      });
+    } else if (imgCoord === rpsCoord.scissor) {
+      this.setState({
+        imgCoord: rpsCoord.paper
+      });
+    } else if (imgCoord === rpsCoord.paper) {
+      this.setState({
+        imgCoord: rpsCoord.rock
+      });
+    }
+  };
 
   render() {
     const { result, score, imgCoord } = this.state;
@@ -67,7 +103,7 @@ class RockPaperScissors extends Component {
         <div>
           <button id="scissor" className="btn" onClick={() => this.onClickBtn('scissor')}>가위</button>
           <button id="rock" className="btn" onClick={() => this.onClickBtn('rock')}>바위</button>
-          <button id="paper" className="btn" onClick={() => this.onClickBtn('paper')}>바위</button>
+          <button id="paper" className="btn" onClick={() => this.onClickBtn('paper')}>보</button>
         </div>
         <div>{result}</div>
         <div>현재 {score}점</div>
