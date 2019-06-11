b345c10377583b05ea70a77180a02f9521a15b25
8c6t <nkw8620@gmail.com>
2019-06-12 00:31:29 +0900

    6-2. setTimeout 여러번 사용하기

--- ch06/Lotto/LottoClass.jsx
+++ ch06/Lotto/LottoClass.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ball from './Ball';
+import { clearTimeout } from 'timers';
 
 function getWinNumbers() {
   console.log('getWinNumbers');
@@ -21,7 +22,34 @@ class LottoClass extends Component {
     redo: false,
   };
 
-  componentDidMount
+  timeouts = [];
+
+  componentDidMount() {
+    const { winNumbers } = this.state;
+
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          }
+        });
+      }, (i + 1) * 1000);
+    }
+
+    setTimeout(() => {
+      this.timeouts[6] = this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  };
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
 
   render() {
     const { winBalls, bonus, redo } = this.state;
@@ -33,7 +61,7 @@ class LottoClass extends Component {
         </div>
         <div>보너스</div>
         {bonus && <Ball number={bonus} />}
-        <button onClick={redo ? this.onClickRedo : () => {}}>한 번 더!</button>
+        { redo && <button onClick={this.onClickRedo}>한 번 더!</button> }
       </>
     );
   }
