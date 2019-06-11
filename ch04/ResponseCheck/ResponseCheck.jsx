fe51490f183c2ca65e8f79d41398b8be688e95db
8c_6t <nkw8620@gmail.com>
2019-06-11 11:18:23 +0900

    4-4. 반응속도체크 Hooks로 전환하기

---
+++ ch04/ResponseCheck/ResponseCheck.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useRef } from 'react';
+
+const ResponseCheck = () => {
+
+  const [ state, setState ] = useState('waiting');
+  const [ message, setMessage ] = useState('클릭해서 시작하세요');
+  const [ results, setResults ] = useState([]);
+
+  // 리렌더링을 하지 않지만 값이 자주 바뀌는 경우 ref를 사용
+  const timeout = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
+
+  const onClickScreen = () => {
+    if (state === 'waiting') {
+      setState('ready');
+      setMessage('초록색이 되면 클릭하세요');
+
+      timeout.current = setTimeout(() => {
+        setState('now');
+        setMessage('지금 클릭');
+        startTime.current = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+
+    } else if (state === 'ready') {
+      setState('waiting');
+      setMessage('초록색일 때 클릭하세요');
+      clearTimeout(this.timeout);
+    } else if (state === 'now') {
+      endTime.current = new Date();
+
+      setState('waiting');
+      setMessage('클릭해서 시작하세요');
+      setResults(prevResult => (
+        [...prevResult, endTime.current - startTime.current]
+      ));
+    }
+  }
+
+  const onReset = () => {
+    setResults([]);
+  };
+
+  const renderAverage = () => (
+    results.length === 0
+      ? null
+      : <>
+        <div>평균 시간: {results.reduce((a, c) => a + c) / results.length}ms</div>
+        <button onClick={onReset}>리셋</button>
+      </>
+  );
+
+  return (
+    <>
+      <div
+        id="screen"
+        className={state} 
+        onClick={onClickScreen}
+      >
+        {message}
+      </div>
+      { renderAverage() }
+    </>
+  );
+
+};
+
+export default ResponseCheck;
--- ch04/ResponseCheck/client.jsx
+++ ch04/ResponseCheck/client.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
 import ResponseCheckClass from './ResponseCheckClass';
+import ResponseCheck from './ResponseCheck';
 
-const Hot = hot(ResponseCheckClass)
+const Hot = hot(ResponseCheck)
 
 ReactDOM.render(<Hot />, document.querySelector('#root'));
