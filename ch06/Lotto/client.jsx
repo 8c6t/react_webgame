f8b5705ebf5ff0209ae3e03f2f5bbead1969f762
8c6t <nkw8620@gmail.com>
2019-06-12 01:19:55 +0900

    6-4. useEffect로 업데이트 감지하기

--- ch06/Lotto/client.jsx
+++ ch06/Lotto/client.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
 import LottoClass from './LottoClass';
+import Lotto from './Lotto';
 
-const Hot = hot(LottoClass);
-
+const Hot = hot(Lotto);
 
 ReactDOM.render(<Hot />, document.querySelector('#root'));
