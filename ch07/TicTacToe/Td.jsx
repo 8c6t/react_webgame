1dfcf13833bd782e151e0e97a84608ce31f09c01
8c6t <nkw8620@gmail.com>
2019-07-24 02:09:31 +0900

    7-4. 틱택토 구현하기

--- ch07/TicTacToe/Td.jsx
+++ ch07/TicTacToe/Td.jsx
@@ -3,9 +3,11 @@ import { CLICK_CELL, CHANGE_TURN } from './TicTacToe';
 
 const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
   const onClickTd = useCallback(() => {
+    if (cellData) {
+      return;
+    }
     dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
-    dispatch({ type: CHANGE_TURN });
-  }, []);
+  }, [cellData]);
 
   return (
     <td onClick={onClickTd}>{cellData}</td>
